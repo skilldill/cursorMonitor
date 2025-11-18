cf2ab1c54f603f47208bb46596d516f8048e1336
Alexander <skilldill@yandex.ru>
2025-11-18 19:49:49 +0300

    Initial Commit

---
+++ customCursorPro/customCursorProApp.swift
@@ -0,0 +1,17 @@
+//
+//  customCursorProApp.swift
+//  customCursorPro
+//
+//  Created by Alexander on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct customCursorProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ customCursorProTests/customCursorProTests.swift
@@ -0,0 +1,17 @@
+//
+//  customCursorProTests.swift
+//  customCursorProTests
+//
+//  Created by Alexander on 18.11.2025.
+//
+
+import Testing
+@testable import customCursorPro
+
+struct customCursorProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
